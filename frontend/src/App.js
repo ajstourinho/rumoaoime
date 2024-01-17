194dac1d21d8eb1fb747f726a25728442e7713f8
ajstourinho <ajstourinho@gmail.com>
2024-01-17 17:08:36 -0300

    Register test in mathjax

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,11 @@ import MathJax from "mathjax3-react";
 import Typography from '@mui/material/Typography';
 
 function App() {
+
+  var html_test = `
+    Seja \\( S = \\left(1+\\operatorname{tg}(\\alpha)+\\sec(\\alpha)\\right)\\left(1+\\operatorname{cotg}(\\alpha)-\\operatorname{cossec}(\\alpha)\\right) \\).
+    Mostre que o valor de \\( S \\) é um número inteiro para todo valor do ângulo \\( \\alpha \\) diferente de \\( \\frac{k\\pi}{2} \\), com \\( k \\in \\mathbb{Z} \\) e calcule esse valor.
+  `
   return (
     <div>
       <MathJax.Provider
@@ -14,6 +19,10 @@ function App() {
 
         <Typography variant="h4">
           <MathJax.Html html="$\ce{CO2 + C -> 2 CO}$" />
+        </Typography>
+
+        <Typography variant="h6">
+          <MathJax.Html html={html_test} />
         </Typography>
 
       </MathJax.Provider>
