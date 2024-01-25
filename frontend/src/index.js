0b27570767613a7c0c2a38e25de440bcaf08b292
ajstourinho <ajstourinho@gmail.com>
2024-01-25 18:01:24 -0300

    Import css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import "./index.css"
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
