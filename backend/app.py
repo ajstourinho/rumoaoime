3670c30bab655d94e6279b20c605b43a8f879614
ajstourinho <ajstourinho@gmail.com>
2024-01-25 18:00:52 -0300

    Configure CORS and test route

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
     return jsonify({"message": "Hello, world! This is the Flask backend."})
+
+@app.route('/data', methods=['GET', 'POST'])
+def post_data():
+    # Handle POST request
+    data = request.json
+    print(data)  # Do something with the data
+    return jsonify({'message': 'POST request successful'})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
