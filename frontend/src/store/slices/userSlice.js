f49f31d781446e58e9a006f612ed2a63fbaa3209
ajstourinho <ajstourinho@gmail.com>
2024-01-29 17:47:09 -0300

    Update userSlice for logInUser

--- frontend/src/store/slices/userSlice.js
+++ frontend/src/store/slices/userSlice.js
@@ -2,20 +2,27 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   isLoggedIn: false,
-  username: ""
+  username: "",
+  email: "",
+  pictureUrl: ""
 }
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    logIn: (state, { payload }) => {
-        return {...state, isLoggedIn: true, user: payload}
+    logInUser: (state, { payload }) => {
+        return {
+          isLoggedIn: true,
+          username: payload.username,
+          email: payload.email,
+          pictureUrl: payload.pictureUrl,
+        }
     },
   },
 })
 
-export const { logIn } = userSlice.actions
+export const { logInUser } = userSlice.actions
 
 export const selectUser = (state) => state.user
 
