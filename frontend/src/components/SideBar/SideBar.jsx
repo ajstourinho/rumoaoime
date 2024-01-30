e930b46c83860fd6303a8634a2bfc17c2b3959b2
ajstourinho <ajstourinho@gmail.com>
2024-01-30 18:22:54 -0300

    Add Tooltip to "Meu progresso"

--- frontend/src/components/SideBar/SideBar.jsx
+++ frontend/src/components/SideBar/SideBar.jsx
@@ -5,6 +5,10 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
+
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../store/slices/userSlice';
 
 import sections from './sections';
 
@@ -19,35 +23,45 @@ const sidebarStyle = {
 };
 
 const Sidebar = () => {
+  const { isLoggedIn } = useSelector(selectUser);
+  
   return (
     <div style={sidebarStyle}>
-      <List
-        sx={{marginBottom: "20px"}}
-      >
-        {
-          sections.map((section, idx) =>
-            <React.Fragment key={idx}>
-              <ListItem style={{ marginBottom: '2px' }}>
-                <Typography variant="overline">
-                  {section.name}
-                </Typography>
-              </ListItem>
+      <List sx={{ marginBottom: "20px" }}>
+        {sections.map((section, idx) => (
+          <React.Fragment key={idx}>
+            <ListItem style={{ marginBottom: "2px" }}>
+              <Typography variant="overline">{section.name}</Typography>
+            </ListItem>
 
-              {
-                section.pages.map((page, idx) =>                 
-                  <ListItem button style={{ height: "35px"}} key={idx}>
-                    <ListItemIcon>
-                      {page.icon}
-                    </ListItemIcon>
+            {section.pages.map((page, idx) => (
+              <Tooltip
+                title={
+                  page.name === "Meu progresso" && !isLoggedIn ?
+                  "É preciso entrar com sua conta para ver o seu progresso."
+                  : false
+                }
+                arrow placement="right-start" style={{cursor: "pointer"}}
+              >
+                <span>
+                  <ListItem
+                    button
+                    style={{ height: "35px" }}
+                    key={idx}
+                    disabled={
+                      page.name === "Meu progresso" && !isLoggedIn ? true : false
+                    }
+                  >
+                    <ListItemIcon>{page.icon}</ListItemIcon>
                     <ListItemText primary={page.name} />
                   </ListItem>
-                )
-              }
-              
-              <Divider style={{ marginBottom: "10px", marginTop: "10px"}}/>
-            </React.Fragment>
-          )
-        }
+                </span>
+              </Tooltip>
+            ))}
+
+            <Divider style={{ marginBottom: "10px", marginTop: "10px" }} />
+          </React.Fragment>
+        ))}
       </List>
     </div>
   );
