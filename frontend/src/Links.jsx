e9996281de987be2e4ccf2e0815437089f473fb1
ajstourinho <ajstourinho@gmail.com>
2024-01-31 16:09:31 -0300

    Login Protect route "/meu-progresso"

--- frontend/src/Links.jsx
+++ frontend/src/Links.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 
 import TesteAxios from "./pages/TesteAxios"
 import TesteMarkdownMathJax from "./pages/TesteMarkdownMathJax"
 import TesteMathJax from './pages/TesteMathJax';
 import LandingPage from './pages/LandingPage';
+import MeuProgresso from './pages/MeuProgresso';
 
 function Links() {
+
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+
+      <Route
+        path="/meu-progresso"
+        element={
+          <ProtectedRoute>
+            <MeuProgresso />
+          </ProtectedRoute>
+        }
+      />
       <Route path="/axios" element={<TesteAxios />} />
       <Route path="/markdown-mathjax" element={<TesteMarkdownMathJax />} />
       <Route path="/mathjax" element={<TesteMathJax />} />
