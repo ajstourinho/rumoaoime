79760e85fdb20688f23600eddac4b5e0e77a5498
ajstourinho <ajstourinho@gmail.com>
2024-02-01 19:07:57 -0300

    Create test display of ButtonCards

--- frontend/src/pages/provasAnteriores/ProvasAnterioresObjetivas.jsx
+++ frontend/src/pages/provasAnteriores/ProvasAnterioresObjetivas.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useTheme } from "@mui/material";
 import CustomUnderlineTypography from "../../components/CustomUnderlineTypography/CustomUnderlineTypography";
 
+import ButtonCard from "../../components/ButtonCard/ButtonCard";
+import ButtonCardsContainer from "../../components/ButtonCardsContainer/ButtonCardsContainer";
+
 const ProvasAnterioresObjetivas = () => {
   const theme = useTheme();
 
@@ -13,6 +16,19 @@ const ProvasAnterioresObjetivas = () => {
       >
         Provas Anteriores - Objetivas
       </CustomUnderlineTypography>
+
+      <ButtonCardsContainer>
+        {
+          [1, 2, 3, 4, 5, 6, 7].map(() =>
+            <ButtonCard
+              circleColor={theme.palette.rumoaoimeCustomColors.lightGrey}
+              title="Título"
+              content="Texto"
+            />
+          )
+        }
+      </ButtonCardsContainer>
+
     </>
   );
 };
