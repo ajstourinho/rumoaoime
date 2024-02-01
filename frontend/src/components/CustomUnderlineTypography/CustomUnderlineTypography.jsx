8d7b36b5829b00f37ed426746fe57419a6213f2f
ajstourinho <ajstourinho@gmail.com>
2024-02-01 16:17:03 -0300

    Create custom colored underline

---
+++ frontend/src/components/CustomUnderlineTypography/CustomUnderlineTypography.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Typography, Box } from "@mui/material";
+
+const CustomUnderlineTypography = ({ children, underlineColor, variant }) => {
+  return (
+    <Box sx={{ display: "inline-block", position: "relative" }}>
+      <Typography variant={variant}>{children}</Typography>
+      <Box
+        sx={{
+          position: "absolute",
+          bottom: -5,
+          left: 0,
+          width: "100%",
+          height: "6px", // Customize the thickness of the underline
+          backgroundColor: underlineColor, // Custom color for the underline
+        }}
+      />
+    </Box>
+  );
+};
+
+export default CustomUnderlineTypography;
