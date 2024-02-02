ab0af749ab29d702c303f704117b03f8015ff906
ajstourinho <ajstourinho@gmail.com>
2024-02-02 17:48:34 -0300

    Create page

---
+++ frontend/src/components/ProvaObjetiva/ProvaObjetiva.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+
+import PageTemplate from '../PageTemplate/PageTemplate';
+import QuestaoObjetivaDisplay from '../QuestaoObjetivaDisplay/QuestaoObjetivaDisplay';
+import PainelProvaObjetiva from "../PainelProvaObjetiva/PainelProvaObjetiva";
+
+import { useParams } from "react-router-dom";
+import { useTheme } from "@mui/material";
+
+const ProvaObjetiva = () => {
+  const { yearYearPlusOne } = useParams();
+  const theme = useTheme();
+
+  return (
+    <>
+      <PageTemplate
+        pageTitle={`Provas Anteriores - Objetivas - ${yearYearPlusOne.replace(
+          "-",
+          "/"
+        )}`}
+        underlineColor={theme.palette.rumoaoimeCustomColors.lightGrey}
+      >
+        
+        <Grid container spacing={2}>
+          <Grid item xs={9}>
+            <QuestaoObjetivaDisplay />
+          </Grid>
+          <Grid item xs={3}>
+            <PainelProvaObjetiva />
+          </Grid>
+        </Grid>
+      </PageTemplate>
+    </>
+  );
+}
+
+export default ProvaObjetiva;
