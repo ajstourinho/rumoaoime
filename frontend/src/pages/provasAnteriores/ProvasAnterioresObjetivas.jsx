2a1c8d4ae9729044611e03cd82c3f332edebebc4
ajstourinho <ajstourinho@gmail.com>
2024-02-02 17:49:32 -0300

    Adjust to page template

--- frontend/src/pages/provasAnteriores/ProvasAnterioresObjetivas.jsx
+++ frontend/src/pages/provasAnteriores/ProvasAnterioresObjetivas.jsx
@@ -1,34 +1,34 @@
 import React from "react";
 import { useTheme } from "@mui/material";
-import CustomUnderlineTypography from "../../components/CustomUnderlineTypography/CustomUnderlineTypography";
 
 import ButtonCard from "../../components/ButtonCard/ButtonCard";
 import ButtonCardsContainer from "../../components/ButtonCardsContainer/ButtonCardsContainer";
+import PageTemplate from '../../components/PageTemplate/PageTemplate';
 
 const ProvasAnterioresObjetivas = () => {
   const theme = useTheme();
 
+  const years = [2023, 2022, 2021, 2020, 2019]
+
   return (
     <>
-      <CustomUnderlineTypography
-        variant="h4"
+      <PageTemplate
+        pageTitle="Provas Anteriores - Objetivas"
         underlineColor={theme.palette.rumoaoimeCustomColors.lightGrey}
       >
-        Provas Anteriores - Objetivas
-      </CustomUnderlineTypography>
-
-      <ButtonCardsContainer>
-        {
-          [1, 2, 3, 4, 5, 6, 7].map(() =>
+        <ButtonCardsContainer>
+          {years.map((year, idx) => (
             <ButtonCard
+              key={idx}
               circleColor={theme.palette.rumoaoimeCustomColors.lightGrey}
-              title="Título"
-              content="Texto"
+              title={year}
+              subtitle={`/ ${year + 1}`}
+              content=""
+              pathTo={`/provas-anteriores/objetivas/${year}-${year + 1}`}
             />
-          )
-        }
-      </ButtonCardsContainer>
-
+          ))}
+        </ButtonCardsContainer>
+      </PageTemplate>
     </>
   );
 };
