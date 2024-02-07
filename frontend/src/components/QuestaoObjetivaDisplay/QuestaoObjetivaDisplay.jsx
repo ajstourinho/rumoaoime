0439ae5c849dee2de398ecda8eaa3c74f039f460
ajstourinho <ajstourinho@gmail.com>
2024-02-06 23:10:35 -0300

    Add previous and next question buttons

--- frontend/src/components/QuestaoObjetivaDisplay/QuestaoObjetivaDisplay.jsx
+++ frontend/src/components/QuestaoObjetivaDisplay/QuestaoObjetivaDisplay.jsx
@@ -2,19 +2,27 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import { Stack } from "@mui/material";
+// Import MUI icons
+import ArrowBackIos from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIos from "@mui/icons-material/ArrowForwardIos";
+
+import ColorButton from "../ColorButton/ColorButton";
+import { grey } from "@mui/material/colors";
 
 const QuestaoObjetivaDisplay = () => {
+
   // Styling for the outer box
   const outerBoxStyle = {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
-    border: "1px solid lightgrey",
+    border: "2px solid lightgrey",
     borderRadius: "8px", // slightly rounded corners
     padding: "20px",
     backgroundColor: "white",
-    height: "100%"
+    height: "100%",
+    // boxShadow: "0 0 10px 0 lightgrey",
   };
 
   // Styling for the inner box
@@ -34,12 +42,32 @@ const QuestaoObjetivaDisplay = () => {
   return (
     <Box sx={outerBoxStyle}>
       <Box sx={innerBoxStyle}>{/* Content of the inner box goes here */}</Box>
-      <Stack direction="row" spacing={2} justifyContent="center">
-        {buttons.map((label) => (
-          <Button key={label} variant="outlined">
-            {label}
-          </Button>
-        ))}
+      <Stack direction="row" spacing={10} justifyContent="center">
+        <ColorButton
+          color="white"
+          backgroundColor={grey[500]}
+          hoverBackgroundColor={grey[700]}
+        >
+          <ArrowBackIos />
+        </ColorButton>
+
+        <Box>
+          <Stack direction="row" spacing={2} justifyContent="center">
+            {buttons.map((label) => (
+              <Button key={label} variant="outlined">
+                {label}
+              </Button>
+            ))}
+          </Stack>
+        </Box>
+
+        <ColorButton
+          color="white"
+          backgroundColor={grey[500]}
+          hoverBackgroundColor={grey[700]}
+        >
+          <ArrowForwardIos />
+        </ColorButton>
       </Stack>
     </Box>
   );
