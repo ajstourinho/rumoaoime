f260807df0df427310024c6eb8807a5cb1c2c8b0
ajstourinho <ajstourinho@gmail.com>
2024-02-08 11:42:35 -0300

    Remove unused routes

--- frontend/src/Links.jsx
+++ frontend/src/Links.jsx
@@ -2,24 +2,12 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 
-import TesteAxios from "./pages/TesteAxios"
-import TesteMarkdownMathJax from "./pages/TesteMarkdownMathJax"
-import TesteMathJax from './pages/TesteMathJax';
 import LandingPage from './pages/LandingPage';
 import MeuProgresso from './pages/MeuProgresso';
-import Matematica from './pages/conteudos/Matematica';
-import Fisica from './pages/conteudos/Fisica';
-import Quimica from './pages/conteudos/Quimica';
-import Portugues from './pages/conteudos/Portugues';
-import Ingles from './pages/conteudos/Ingles';
-import Redacao from './pages/conteudos/Redacao';
+
 import ProvasAnterioresObjetivas from './pages/provasAnteriores/ProvasAnterioresObjetivas';
 import ProvasAnterioresDiscursivas from './pages/provasAnteriores/ProvasAnterioresDiscursivas';
-import SimuladosObjetivos from './pages/simulados/SimuladosObjetivos';
-import SimuladosDiscursivos from './pages/simulados/SimuladosDiscursivos';
-import SobreOIME from './pages/SobreOIME';
-import SobreOSite from './pages/SobreOSite';
-import ProvaObjetiva from './components/ProvaObjetiva/ProvaObjetiva';
+import ProvaObjetiva from './pages/provasAnteriores/ProvaObjetiva';
 
 function Links() {
 
@@ -36,18 +24,6 @@ function Links() {
         }
       />
 
-      {/* TopBar */}
-      {/* <Route path="/sobre-o-ime/" element={<SobreOIME />} />
-      <Route path="/sobre-o-site/" element={<SobreOSite />} /> */}
-
-      {/* Conteúdos */}
-      {/* <Route path="/conteudos/matematica" element={<Matematica />} />
-      <Route path="/conteudos/fisica" element={<Fisica />} />
-      <Route path="/conteudos/quimica" element={<Quimica />} />
-      <Route path="/conteudos/portugues" element={<Portugues />} />
-      <Route path="/conteudos/ingles" element={<Ingles />} />
-      <Route path="/conteudos/redacao" element={<Redacao />} /> */}
-
       {/* Provas Anteriores */}
       <Route
         path="/provas-anteriores/objetivas"
@@ -62,15 +38,6 @@ function Links() {
         path="/provas-anteriores/discursivas"
         element={<ProvasAnterioresDiscursivas />}
       />
-
-      {/* Simulados */}
-      {/* <Route path="/simulados/objetivos" element={<SimuladosObjetivos />} />
-      <Route path="/simulados/discursivos" element={<SimuladosDiscursivos />} /> */}
-
-      {/* Testes */}
-      {/* <Route path="/axios" element={<TesteAxios />} />
-      <Route path="/markdown-mathjax" element={<TesteMarkdownMathJax />} />
-      <Route path="/mathjax" element={<TesteMathJax />} /> */}
 
       <Route path="*" element={<LandingPage />} />
     </Routes>
