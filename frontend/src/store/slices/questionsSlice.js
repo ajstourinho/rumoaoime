f59088ac36baa934feb42c4ba951bb115ccaac3e
ajstourinho <ajstourinho@gmail.com>
2024-02-08 15:11:52 -0300

    Create unsetCurrent function

--- frontend/src/store/slices/questionsSlice.js
+++ frontend/src/store/slices/questionsSlice.js
@@ -1,34 +1,50 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialQuestionsState = {};
 const start = 1;
 const end = 40;
 for (let i = start; i <= end; i++) {
-  initialQuestionsState[i] = "notSeen";
+  initialQuestionsState[i] = {
+    status: "notSeen",
+  };
 }
 
-const localStorageQuestionsState = localStorage.getItem('questionsState')
+const localStorageQuestionsState = localStorage.getItem("questionsState");
 
-const conditionalInitialState = (localStorageQuestionsState === null) ? initialQuestionsState : JSON.parse(localStorageQuestionsState)
+const conditionalInitialState =
+  localStorageQuestionsState === null
+    ? initialQuestionsState
+    : JSON.parse(localStorageQuestionsState);
 
 export const questionsSlice = createSlice({
-  name: 'questions',
+  name: "questions",
   initialState: conditionalInitialState,
   reducers: {
     changeQuestionState: (state, { payload }) => {
-
       const newState = state;
       newState[payload.questionNumber] = payload.questionState;
-      
-      localStorage.setItem('questionsState', JSON.stringify(newState))
 
-      return newState
+      localStorage.setItem("questionsState", JSON.stringify(newState));
+
+      return newState;
+    },
+    unsetCurrent: (state) => {
+      const newState = state;
+
+      for (const key in newState) {
+        if (newState[key].status == "current") {
+          newState[key] = { ...newState[key], status: "seen" };
+          break;
+        }
+      }
+
+      return newState;
     },
   },
-})
+});
 
-export const { changeQuestionState } = questionsSlice.actions
+export const { changeQuestionState, unsetCurrent } = questionsSlice.actions;
 
-export const selectQuestions = (state) => state.questions
+export const selectQuestions = (state) => state.questions;
 
-export default questionsSlice.reducer
+export default questionsSlice.reducer;
