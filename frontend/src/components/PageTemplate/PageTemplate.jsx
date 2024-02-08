ce7c40f63ae6e08391946582a2b1d986f82200f9
ajstourinho <ajstourinho@gmail.com>
2024-02-08 11:43:55 -0300

    Adjust grid layout

--- frontend/src/components/PageTemplate/PageTemplate.jsx
+++ frontend/src/components/PageTemplate/PageTemplate.jsx
@@ -1,16 +1,31 @@
 import React from "react";
 
 import CustomUnderlineTypography from "../CustomUnderlineTypography/CustomUnderlineTypography";
+import { Grid, Stack } from '@mui/material';
 
-const PageTemplate = ({ pageTitle, underlineColor, children }) => {
+const PageTemplate = ({ pageTitle, underlineColor, children, sideComponent }) => {
 
   return (
     <>
-      <CustomUnderlineTypography variant="h4" underlineColor={underlineColor}>
-        {pageTitle}
-      </CustomUnderlineTypography>
+      <Grid container>
+        <Grid container item xs={9}>
 
-      {children}
+            <Grid item xs sx={{mb: 2 }}>
+              <CustomUnderlineTypography
+                variant="h4"
+                underlineColor={underlineColor}
+              >
+                {pageTitle}
+              </CustomUnderlineTypography>
+            </Grid>
+
+            {children}
+        </Grid>
+
+        <Grid item xs={3}>
+          {sideComponent}
+        </Grid>
+      </Grid>
     </>
   );
 }
