9cef0ad7b0ce96b3b248090fd2658bdc46b4e83a
ajstourinho <ajstourinho@gmail.com>
2024-02-08 11:44:07 -0300

    Remove unused sections

--- frontend/src/components/SideBar/sections.js
+++ frontend/src/components/SideBar/sections.js
@@ -24,47 +24,6 @@ const sections = [
             },
         ]
     },
-    // {
-    //     name: "Conteúdos",
-    //     pages: [
-    //         {
-    //             name: "Matemática",
-    //             icon: <CalculateIcon />,
-    //             navigateTo: "/conteudos/matematica",
-    //             selectedColor: theme.palette.rumoaoimeCustomColors.matematica
-    //         },
-    //         {
-    //             name: "Física",
-    //             icon: <PsychologyIcon />,
-    //             navigateTo: "/conteudos/fisica",
-    //             selectedColor: theme.palette.rumoaoimeCustomColors.fisica
-    //         },
-    //         {
-    //             name: "Química",
-    //             icon: <ScienceIcon />,
-    //             navigateTo: "/conteudos/quimica",
-    //             selectedColor: theme.palette.rumoaoimeCustomColors.quimica
-    //         },
-    //         {
-    //             name: "Português",
-    //             icon: <AbcIcon />,
-    //             navigateTo: "/conteudos/portugues",
-    //             selectedColor: theme.palette.rumoaoimeCustomColors.portugues
-    //         },
-    //         {
-    //             name: "Inglês",
-    //             icon: <LanguageIcon />,
-    //             navigateTo: "/conteudos/ingles",
-    //             selectedColor: theme.palette.rumoaoimeCustomColors.ingles
-    //         },
-    //         {
-    //             name: "Redação",
-    //             icon: <DriveFileRenameOutlineIcon />,
-    //             navigateTo: "/conteudos/redacao",
-    //             selectedColor: theme.palette.rumoaoimeCustomColors.redacao
-    //         },
-    //     ]
-    // },
     {
         name: "Provas anteriores",
         pages: [
@@ -72,33 +31,16 @@ const sections = [
                 name: "Objetivas",
                 icon: <RuleIcon />,
                 navigateTo: "/provas-anteriores/objetivas",
-                selectedColor: theme.palette.rumoaoimeCustomColors.lightGrey
+                selectedColor: theme.palette.rumoaoimeCustomColors.darkGrey
             },
             {
                 name: "Discursivas",
                 icon: <EditNoteIcon />,
                 navigateTo: "/provas-anteriores/discursivas",
-                selectedColor: theme.palette.rumoaoimeCustomColors.lightGrey
+                selectedColor: theme.palette.rumoaoimeCustomColors.darkGrey
             },
         ]
     },
-    // {
-    //     name: "Simulados",
-    //     pages: [
-    //         {
-    //             name: "Objetivos",
-    //             icon: <AssignmentTurnedInIcon />,
-    //             navigateTo: "/simulados/objetivos",
-    //             selectedColor: theme.palette.rumoaoimeCustomColors.lightGrey
-    //         },
-    //         {
-    //             name: "Discursivos",
-    //             icon: <AssignmentIcon />,
-    //             navigateTo: "/simulados/discursivos",
-    //             selectedColor: theme.palette.rumoaoimeCustomColors.lightGrey
-    //         },
-    //     ]
-    // },
 ];
 
 export default sections;
