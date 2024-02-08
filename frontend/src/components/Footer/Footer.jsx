4332daa93fc24e317e3b416954a24e1014bff929
ajstourinho <ajstourinho@gmail.com>
2024-02-08 11:43:41 -0300

    Base css on custom theme value

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 
 import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material';
 
-const footerStyle = {
-  textAlign: 'center',
-  padding: '10px',
-  background: 'white',
-  color: '#888',
-  position: 'fixed',
-  bottom: 0,
-  left: 0,
-  right: 0,
-  width: '100%',
-  borderTop: '2px solid #ccc', // Add an upper border
-  height: "20px"
-};
 
 const Footer = () => {
+  const theme = useTheme();
+  
+  const footerStyle = {
+    textAlign: 'center',
+    padding: '10px',
+    background: 'white',
+    color: '#888',
+    position: 'fixed',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: '100%',
+    borderTop: '2px solid #ccc', // Add an upper border
+    height: theme.custom.footerHeight,
+  };
+  
   return (
     <footer style={footerStyle}>
         <Typography variant="caption">
